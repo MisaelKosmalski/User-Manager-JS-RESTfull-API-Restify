8b2e2262422f47afb585d60605bdbaa0ef557122
Misael Kosmalski <misaelkosmalski@gmail.com>
2023-07-31 00:12:01 -0300

    using Restify to access API Rest

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var router = express.Router();
+var assert = require('assert');
+var restify = require('restify-clients');
+var router = express.Router();  
+
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://127.0.0.1:4000'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  client.get('/users', function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
+
 });
 
 module.exports = router;
